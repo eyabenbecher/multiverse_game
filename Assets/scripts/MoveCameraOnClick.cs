3181f807cb1341c5b0149ddd85a39a183be7d4e3
eyabenbecher <150703203+eyabenbecher@users.noreply.github.com>
2023-12-30 16:51:12 +0100

    second

--- Assets/scripts/MoveCameraOnClick.cs
+++ Assets/scripts/MoveCameraOnClick.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Cinemachine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MoveCameraOnClick : MonoBehaviour, IPointerClickHandler
 {
@@ -9,12 +10,15 @@ public class MoveCameraOnClick : MonoBehaviour, IPointerClickHandler
     public CinemachineVirtualCamera virtualCamera;
 
    
-    public Transform targetPosition;
+    public Transform targetPositionmars;
+    public Transform targetPositionvenus;
 
-    
-    public float followSpeed = 50f;
+
+    public float followSpeed = 4000f;
     public GameObject uiElement1;
     public GameObject uiElement2;
+    public GameObject uiElement3;
+    public GameObject uiElement4;
 
 
 
@@ -23,14 +27,48 @@ public class MoveCameraOnClick : MonoBehaviour, IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
         
-        MoveCamera();
+        MoveCameramars();
 
         uiElement1.SetActive(isUIVisible);
         uiElement2.SetActive(isUIVisible);
+        uiElement3.SetActive(true);
+    }
+    public void OnButtonClickvenus()
+    {
+        MoveCameravenus();
+        uiElement3.SetActive(false);
+        uiElement4.SetActive(true);
+
+
+
+    }
+    public void OnButtonClickmars()
+    {
+        MoveCameramars();
+        uiElement3.SetActive(true);
+        uiElement4.SetActive(false);
+
+
+
+    }
+    public void OnButtonClickvisitmars()
+    {
+
+
+        SceneManager.LoadScene("mars");
+
+    }
+    public void OnButtonClickvisitvenus()
+    {
+
+
+        SceneManager.LoadScene("venus");
+
     }
 
-    
-    private void MoveCamera()
+
+
+    private void MoveCameramars()
     {
         
         if (virtualCamera != null)
@@ -46,11 +84,37 @@ public class MoveCameraOnClick : MonoBehaviour, IPointerClickHandler
             }
 
             
-            virtualCamera.transform.position = Vector3.Lerp(virtualCamera.transform.position, targetPosition.position, Time.deltaTime * followSpeed);
+            virtualCamera.transform.position = Vector3.Lerp(virtualCamera.transform.position, targetPositionmars.position, Time.deltaTime * followSpeed);
         }
         else
         {
             Debug.LogError("Virtual Camera not assigned!");
+
         }
     }
+    private void MoveCameravenus()
+    {
+
+        if (virtualCamera != null)
+        {
+
+
+            CinemachineTransposer transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+            if (transposer != null)
+            {
+
+
+                transposer.m_FollowOffset = new Vector3(0, 0, 0);
+            }
+
+
+            virtualCamera.transform.position = Vector3.Lerp(virtualCamera.transform.position, targetPositionvenus.position, Time.deltaTime * followSpeed);
+        }
+        else
+        {
+            Debug.LogError("Virtual Camera not assigned!");
+
+        }
+    }
+
 }
